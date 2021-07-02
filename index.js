5e893d6bf021cb7aa9f4bc428fa3aa46cc174113
Djestaz <djestacom@gmail.com>
2021-07-02 15:32:34 +0100

    updated stuff

---
+++ index.js
@@ -0,0 +1,14 @@
+// function displayMessage(name) {
+//     console.log('Hello ' + name)
+// }
+
+// displayMessage(54)
+
+function halloWorld() {
+  const message = "Hallo world";
+  console.log(message);
+  return message;
+}
+halloWorld();
+
+exports.halloWorld = halloWorld;
